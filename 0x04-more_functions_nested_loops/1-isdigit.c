b0949d56c0d59e5565bd58ded4987efea8b28fec
fatmatnani <fatmatnani08@gmail.com>
2022-12-16 10:01:12 -0800

    digit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -5,11 +5,11 @@
  * @c: char to be checked
  * Return: 0 or 1 if c is a digit
  */
-  
- int _isdigit(int c)
+
+int _isdigit(int c)
 {
-	if (c >= 48 && c <= 57)
-		return (1);
-	else
-		return (0);
+if (c >= '0' && c <= '9')
+return (1);
+else
+return (0);
 }
