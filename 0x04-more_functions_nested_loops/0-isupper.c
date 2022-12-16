cf12e1c3e15b2f4384e914a24ddfb72a91d13237
fatmatnani <fatmatnani08@gmail.com>
2022-12-16 04:02:22 -0800

    is uper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,14 +1,13 @@
 # include "main.h"
 
 /**
- * _isuppe - uppercase letters
+ * _isupper - uppercase letters
  * @c: char to be checked
  * Return: 0 or 1 if c is uppercase
  */
 
 int _isupper(int c)
 {
-
 	if (c >= 'A' && c <= 'Z')
 		return (1);
 	else
