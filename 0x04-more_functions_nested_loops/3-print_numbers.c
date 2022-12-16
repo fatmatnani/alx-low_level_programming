6dcc86bdad92d119e10f71c25a0e619453b5aedf
fatmatnani <fatmatnani08@gmail.com>
2022-12-16 11:46:33 -0800

    numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -4,14 +4,13 @@
  * print_numbers - print num from 0-9
  * Return: void
  */
-
 void print_numbers(void)
 {
 	char c;
 
 	for (c = '0'; c <= '9'; c++)
 	{
-		_putchar(c + '0');
+		_putchar(c);
 	}
 	_putchar ('\n');
 }
