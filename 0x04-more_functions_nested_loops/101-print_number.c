6cb0193696828518d97805ae0d47a3c807937b3b
fatmatnani <fatmatnani08@gmail.com>
2022-12-17 16:41:27 -0800

    print number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,15 +7,26 @@
 
 void print_number(int n)
 {
-	unsigned int x;
+	unsigned int num2, num1 = n;
+		int div = 1;
 
-	x = n;
 	if (n < 0)
 	{
-		_putchar(45);
-		x = -n;
+		_putchar('-');
+		num1 = n * -1;
 	}
-	if (x / 10)
-		print_number(x / 10);
-	_putchar((x % 10) + '0');
+	num2 = num1;
+	while (num2 > 9)
+	{
+		div *= 10;
+		num2 = num2 / 10;
+	}
+	while (div >= 1)
+	{
+		num2 = num1 % div;
+		num1 /= div;
+		_putchar(num1 + '0');
+		num1 = num2;
+		div /= 10;
+	}
 }
