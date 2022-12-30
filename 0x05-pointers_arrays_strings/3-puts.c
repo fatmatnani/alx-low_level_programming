b8bfeb6f0c4e57b500ed4bffad6787dbd8ae271f
fatmatnani <fatmatnani08@gmail.com>
2022-12-30 09:00:16 -0800

    task 2

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _puts - print a string to the stdout
+ * @str: the string
+ * Return: the length of the string
+ */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+		_putchar(*str++);
+_putchar('\n');
+}
